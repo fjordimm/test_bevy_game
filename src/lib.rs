1c788488ce1f6727a646a978dc41ebf88cd540b4
fjordimm <fjordimm@gmail.com>
2026-02-25 16:49:21 -0800

    got logging to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{log::LogPlugin, prelude::*};
 
 pub mod core;
 pub mod world;
@@ -6,9 +6,20 @@ pub mod world;
 pub fn build_app() -> App {
     let mut app = App::new();
 
+    #[cfg(debug_assertions)]
+    app.add_plugins(DefaultPlugins
+        .set(LogPlugin {
+            filter: "info,wgpu_core=warn,wgpu_hal=warn,test_bevy_game=trace".into(),
+            level: bevy::log::Level::INFO,
+            ..default()
+        })
+    );
+
+    #[cfg(not(debug_assertions))]
+    app.add_plugins(DefaultPlugins);
+
     #[rustfmt::skip]
     app
-        .add_plugins(DefaultPlugins)
         .add_plugins(core::CorePlugin)
         // .add_plugins(player::PlayerPlugin)
         // .add_plugins(physics::PhysicsPlugin)
--- src/world/plugin.rs
+++ src/world/plugin.rs
@@ -3,5 +3,32 @@ use bevy::prelude::*;
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        #[rustfmt::skip]
+        app
+            .add_systems(Startup, startup);
+    }
+}
+
+fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(255, 0, 0))),
+        Transform::from_xyz(0.0, 0.5, -5.0),
+    ));
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 0.0, 0.0).looking_at(-Vec3::Z, Vec3::Y),
+    ));
 }
