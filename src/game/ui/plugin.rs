2004aec54709cdd31b64dc2d21876018d063e601
fjordimm <fjordimm@gmail.com>
2026-03-14 22:06:05 -0700

    routine

--- src/game/ui/plugin.rs
+++ src/game/ui/plugin.rs
@@ -11,32 +11,8 @@ impl Plugin for UiPlugin {
         #[rustfmt::skip]
         app
             .add_plugins(EguiPlugin::default())
-            // .add_systems(Update, funny1.after(PlayingStateOrdering::Ui));
-        .add_systems(Update, on_creation_of_camera_for_egui)
-        .add_systems(Update, on_creation_of_egui_context);
-    }
-}
-
-fn funny1(
-    cameras: Query<Entity, Added<CameraForEgui>>,
-    mut commands: Commands,
-    mut egui_contexts: Query<&mut EguiContext>,
-) {
-    for camera in &cameras {
-        debug!("camera added");
-        debug!("eguis: {}", egui_contexts.iter().count());
-
-        commands
-            .entity(camera)
-            .insert(EguiMultipassSchedule(CameraForEgui.intern()));
-
-        for mut egui_context in &mut egui_contexts {
-            let ctx = egui_context.get_mut();
-
-            ctx.style_mut(|style| {
-                style.visuals.override_text_color = Some(egui::Color32::from_rgb(0, 128, 0));
-            });
-        }
+            .add_systems(Update, on_creation_of_camera_for_egui)
+            .add_systems(Update, on_creation_of_egui_context);
     }
 }
 
@@ -45,22 +21,18 @@ fn on_creation_of_camera_for_egui(
     mut commands: Commands,
 ) {
     for camera in &cameras {
-        debug!("camera added");
-
         commands
             .entity(camera)
             .insert(EguiMultipassSchedule(CameraForEgui.intern()));
     }
 }
 
-fn on_creation_of_egui_context(mut egui_contexts: Query<&mut EguiContext, Added<CameraForEgui>>) {
+fn on_creation_of_egui_context(mut egui_contexts: Query<&mut EguiContext, Added<EguiContext>>) {
     for mut egui_context in &mut egui_contexts {
-        debug!("context added");
-
         let ctx = egui_context.get_mut();
 
         ctx.style_mut(|style| {
-            style.visuals.override_text_color = Some(egui::Color32::from_rgb(0, 128, 0));
+            style.animation_time = 0.0;
         });
     }
 }
